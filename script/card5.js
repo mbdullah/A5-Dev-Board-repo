6ff3c32782fe74b933c814fa2af6764b98e878f7
mbdullah <mdabdullah162005@gmail.com>
2025-03-01 00:15:40 -0800

    added the card no 5 javascript event, please sent it

---
+++ script/card5.js
@@ -0,0 +1,30 @@
+document.getElementById("five-card-btn").addEventListener("click", function(){
+    // step no 1
+    alert("Board Update Successfully");
+    // step no 2
+    const navBarScore = getInnerTextById ("navigation-bar-score");
+    const navigationScoreSum = navBarScore + 1;
+    const sum = getSum("navigation-bar-score", navigationScoreSum);
+    // step no 3
+    const mainScore = getInnerTextById("main-score");
+    const mainScoreSum = mainScore - 1;
+    const mainSum = getSum("main-score", mainScoreSum);
+    // step 4
+    const childContainer = document.getElementById("child-container");
+    const fiveCardTitle = document.getElementById("five-card-title").innerText;
+    const div = document.createElement('div');
+    div.classList.add("bg-[#F4F7FF]", "rounded-[8px]", "w-[90%]", "mx-auto")
+    const p = document.createElement("p");
+    p.classList.add("p-5", "text-[#0000008e]");
+    function updateTime (){
+        const now = new Date();
+        const timeString = now.toLocaleTimeString();
+        p.innerText = `You have Complete The Task ${fiveCardTitle} at ${timeString}`;
+    }
+    div.appendChild(p);
+    childContainer.appendChild(div);
+    updateTime();
+    // step 5 btn disabled
+    this.disabled = true;
+    this.style.backgroundColor  = "#3952f135";
+})
